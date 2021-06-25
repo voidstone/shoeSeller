18e45a31c64d16141d9374aba9fff88db1c0b139
sage <chaikin.sergey@gmail.com>
2021-06-25 23:06:17 +0400

    add skeleton main page css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hekko</h1>
-      <p>Поясни за шмот</p>
+    <div className="wrapper clear">
+      <header className="d-flex justify-between align-center p-40">
+        <div className="d-flex align-center">
+          <img width={40} height={40} src="/img/logo.png" />
+          <div>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <div>
+          <ul className="d-flex">
+            <li className="mr-30">
+              <img width={18} height={18} src="/img/cart.svg" />
+              <span>1205 руб.</span>
+            </li>
+            <li>
+              <img width={18} height={18} src="/img/user.svg" />
+            </li>
+          </ul>
+        </div>
+      </header>
+      <div className="content p-40">
+        <h1>Все кроссовки</h1>
+        ...
+      </div>
     </div>
   );
 }
