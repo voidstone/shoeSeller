5b236a8e64a7aadeac937512aeea800bbd39a1e0
sage <chaikin>
2021-06-25 16:23:41 +0400

    add sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="App">Hekko</div>;
+  return (
+    <div className="App">
+      <h1>Hekko</h1>
+      <p>Поясни за шмот</p>
+    </div>
+  );
 }
 
 export default App;
