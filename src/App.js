97077c81a81352c336ad14e6e50a38d51fd4b1c4
sage <chaikin>
2021-07-01 10:00:29 +0400

    add cartItem

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ function App() {
     <div className="wrapper clear">
       <div className="overlay">
         <div className="drawer">
-          <h2>Корзина</h2>
-          <div className="cartItem d-flex align-center">
-            <img
-              className="mr-20"
-              width={70}
-              height={70}
-              src="/img/sneakers/1.jpg"
-              alt="Sneakers"
-            />
-            <div className="mr-20">
+          <h2 className="mb-30">Корзина</h2>
+          <div className="cartItem d-flex align-center mb-20">
+            <div
+              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+              className="cartItemImg"
+            ></div>
+            <div className="mr-20 flex">
+              <p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
+              <b>12 999р</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+
+          <div className="cartItem d-flex align-center mb-20">
+            <div
+              style={{ backgroundImage: "url(/img/sneakers/1.jpg)" }}
+              className="cartItemImg"
+            ></div>
+            <div className="mr-20 flex">
               <p className="mb-5">Мужские кроссовки Nike Blazer Mid Suede</p>
               <b>12 999р</b>
             </div>
